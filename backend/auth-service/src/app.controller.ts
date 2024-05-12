b7dafdf2d508fd8e901a88c06379fb7af114f54c
linjunpeng1998 <93981120+linjunpeng1998@users.noreply.github.com>
2024-05-12 11:41:02 +0800

    Update app.controller.ts
    统一两个接口，返回token和用户信息。

--- backend/auth-service/src/app.controller.ts
+++ backend/auth-service/src/app.controller.ts
@@ -5,13 +5,10 @@ import { AppService, LoginVo } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post('/login')
-  login(@Body() vo: LoginVo): string {
-    return this.appService.sign(vo);
-  }
-
-  @Post('/info')
-  getInfo(@Body() token: string): LoginVo {
-    return this.appService.getInfo(token);
+  @Post('/login') // 统一登陆接口
+  async login(@Body() vo: LoginVo): Promise<{ token: string; userInfo: LoginVo }> {
+    const token = await this.appService.sign(vo); // 生成 token
+    const userInfo = await this.appService.getInfo(token); // 获取用户信息
+    return { token, userInfo }; // 返回 token 和用户信息给前端
   }
 }
